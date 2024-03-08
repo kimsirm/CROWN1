0540cff6891bf71aa15b13fc8c7fbdea4ce0dcee
kimsirm <141669743+kimsirm@users.noreply.github.com>
2024-03-08 21:18:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -176,7 +176,7 @@ if (autobio === 'TRUE'){
 
                          client.updateProfileStatus( 
 
-                                         `Long live ${botname}\n\n${date.toLocaleString('en-US', { timeZone: 'Africa/Nairobi' })} It's a ${date.toLocaleString('en-US', { weekday: 'long', timeZone: 'Africa/Nairobi'})}.` 
+                                         `Ifound peace!in debugging'})}.` 
 
                                  ) 
 

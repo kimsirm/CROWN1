69e84f0c23403f198e5eadb0ac5ac798c315f69e
kimsirm <141669743+kimsirm@users.noreply.github.com>
2024-03-08 22:34:57 +0300

    Update dreaded.js

--- dreaded.js
+++ dreaded.js
@@ -93,7 +93,7 @@ const menu = process.env.MENU_TYPE || 'VIDEO';
     const Owner = DevDreaded.map((v) => v.replace(/[^0-9]/g, "") + "@s.whatsapp.net").includes(m.sender)
     // Group
     const groupMetadata = m.isGroup ? await client.groupMetadata(m.chat).catch((e) => {}) : "";
-    const groupName = m.isGroup ? groupMetadata ? await groupMetadata.subject : "";
+    const groupName = m.isGroup && groupMetadata ? await groupMetadata.subject : "";
     const participants = m.isGroup && groupMetadata ? await groupMetadata.participants : ""; 
      const groupAdmin = m.isGroup ? await getGroupAdmins(participants) : ""; 
      const isBotAdmin = m.isGroup ? groupAdmin.includes(botNumber) : false; 
